b0b70bdf6d153adf44da97a02f9eeb199fde158d
Sujeet Kumar <sk7295911@gmail.com>
2026-08-06 23:13:19 +0530

    Add Medium Java Solution from LeetCode

---
+++ LeetCode/Java/Medium/63. Unique Paths II/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length,n=obstacleGrid[0].length;
+        int[][] dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if((i==0 || j==0) && obstacleGrid[i][j]!=1) dp[i][j]=1;
+                else{
+                    if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                    else dp[i][j]=dp[i][j-1]+dp[i-1][j];
+                } 
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
