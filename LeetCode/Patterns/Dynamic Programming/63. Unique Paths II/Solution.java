25de1c6091e2eef7b123dd58192b8f9d7ada91dc
Sujeet Kumar <sk7295911@gmail.com>
2026-08-06 23:30:17 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/63. Unique Paths II/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/63. Unique Paths II/Solution.java
@@ -4,7 +4,8 @@ class Solution {
         int[][] dp=new int[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if((i==0 || j==0) && obstacleGrid[i][j]!=1) dp[i][j]=1;
+                if(obstacleGrid[i][j]==1) dp[i][j]=0;
+                else if((i==0 || j==0) && obstacleGrid[i][j]!=1) dp[i][j]=1;
                 else{
                     if(obstacleGrid[i][j]==1) dp[i][j]=0;
                     else dp[i][j]=dp[i][j-1]+dp[i-1][j];
