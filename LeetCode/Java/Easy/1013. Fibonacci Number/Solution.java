5523283026680a14c956adec7d28968ba9ccbf01
Sujeet Kumar <sk7295911@gmail.com>
2026-08-07 21:02:22 +0530

    Add Easy Java Solution from LeetCode

--- LeetCode/Java/Easy/1013. Fibonacci Number/Solution.java
+++ LeetCode/Java/Easy/1013. Fibonacci Number/Solution.java
@@ -1,8 +1,13 @@
 class Solution {
     public int fib(int n) {
-       int[] dp=new int[n+1];
+       int[] dp=new int[3];
        if(n>0) dp[1]=1;
-       for(int i=2;i<=n;i++) dp[i]=dp[i-1]+dp[i-2];
-       return dp[n];
+       if(n==1) return dp[1];
+       for(int i=2;i<=n;i++){
+            dp[2]=dp[0]+dp[1];
+            dp[0]=dp[1];
+            dp[1]=dp[2];
+       }
+       return dp[2];
     }
 }
