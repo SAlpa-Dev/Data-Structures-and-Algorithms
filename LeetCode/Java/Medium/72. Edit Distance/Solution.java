61027c4b935252416805578884ffa7a65095c8d9
Sujeet Kumar <sk7295911@gmail.com>
2026-08-11 13:56:44 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/72. Edit Distance/Solution.java
+++ LeetCode/Java/Medium/72. Edit Distance/Solution.java
@@ -1,18 +1,21 @@
 class Solution {
-    public int minimum(StringBuilder s1,StringBuilder s2,int i,int j,int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)) return dp[i][j]=minimum(s1,s2,i-1,j-1,dp);
-        int replace=1+minimum(s1,s2,i-1,j-1,dp);
-        int delete=1+minimum(s1,s2,i-1,j,dp);
-        int insert=1+minimum(s1,s2,i,j-1,dp);
-        return dp[i][j]=Math.min(replace,Math.min(delete,insert));
-    }
     public int minDistance(String s1, String s2) {
-        int[][] dp=new int[s1.length()][s2.length()];
-        for(int[] ele:dp) Arrays.fill(ele,-1);
-        int i=s1.length()-1,j=s2.length()-1;
-        return minimum(new StringBuilder(s1),new StringBuilder(s2),i,j,dp);
+        int m=s1.length(),n=s2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<m;i++) dp[i][0]=i;
+        for(int j=0;j<n;j++) dp[0][j]=j;
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j]=dp[i-1][j-1];
+                else{
+                    int replace=1+dp[i-1][j-1];
+                    int delete=1+dp[i-1][j];
+                    int insert=1+dp[i][j-1];
+                    dp[i][j]=Math.min(replace,Math.min(delete,insert));
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
