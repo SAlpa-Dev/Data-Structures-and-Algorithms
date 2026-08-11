e55b273b26df7bf5e3b479d1bdeeda6803035c38
Sujeet Kumar <sk7295911@gmail.com>
2026-08-11 12:24:24 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/72. Edit Distance/Solution.java
+++ LeetCode/Java/Medium/72. Edit Distance/Solution.java
@@ -1,15 +1,18 @@
 class Solution {
-    public int minimum(StringBuilder s1,StringBuilder s2,int i,int j){
+    public int minimum(StringBuilder s1,StringBuilder s2,int i,int j,int[][] dp){
         if(i<0) return j+1;
         if(j<0) return i+1;
-        if(s1.charAt(i)==s2.charAt(j)) return minimum(s1,s2,i-1,j-1);
-        int replace=1+minimum(s1,s2,i-1,j-1);
-        int delete=1+minimum(s1,s2,i-1,j);
-        int insert=1+minimum(s1,s2,i,j-1);
-        return Math.min(replace,Math.min(delete,insert));
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s1.charAt(i)==s2.charAt(j)) return dp[i][j]=minimum(s1,s2,i-1,j-1,dp);
+        int replace=1+minimum(s1,s2,i-1,j-1,dp);
+        int delete=1+minimum(s1,s2,i-1,j,dp);
+        int insert=1+minimum(s1,s2,i,j-1,dp);
+        return dp[i][j]=Math.min(replace,Math.min(delete,insert));
     }
     public int minDistance(String s1, String s2) {
+        int[][] dp=new int[s1.length()][s2.length()];
+        for(int[] ele:dp) Arrays.fill(ele,-1);
         int i=s1.length()-1,j=s2.length()-1;
-        return minimum(new StringBuilder(s1),new StringBuilder(s2),i,j);
+        return minimum(new StringBuilder(s1),new StringBuilder(s2),i,j,dp);
     }
 }
