ccbf2c7d522d9325120d69fbd2549712f90a65c0
Sujeet Kumar <sk7295911@gmail.com>
2026-08-11 13:57:41 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/72. Edit Distance/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/72. Edit Distance/Solution.java
@@ -2,8 +2,8 @@ class Solution {
     public int minDistance(String s1, String s2) {
         int m=s1.length(),n=s2.length();
         int[][] dp=new int[m+1][n+1];
-        for(int i=0;i<m;i++) dp[i][0]=i;
-        for(int j=0;j<n;j++) dp[0][j]=j;
+        for(int i=0;i<=m;i++) dp[i][0]=i;
+        for(int j=0;j<=n;j++) dp[0][j]=j;
 
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
