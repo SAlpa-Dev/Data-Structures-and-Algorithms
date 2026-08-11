e8b77d20537287004ddf283159ad5c750ac7b6b3
Sujeet Kumar <sk7295911@gmail.com>
2026-08-11 13:17:37 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/1250. Longest Common Subsequence/Solution.java
+++ LeetCode/Java/Medium/1250. Longest Common Subsequence/Solution.java
@@ -6,10 +6,14 @@ class Solution {
         else return dp[i][j]=Math.max(length(s1,s2,i-1,j,dp),length(s1,s2,i,j-1,dp));
     }
     public int longestCommonSubsequence(String s1, String s2) {
-        int[][] dp=new int[s1.length()][s2.length()];
-        for(int[]ele:dp) Arrays.fill(ele,-1);
-        int i=s1.length()-1;
-        int j=s2.length()-1;
-        return length(s1,s2,i,j,dp);
+        int m=s1.length(),n=s2.length();
+        int[][] dp=new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[m][n];
     }
 }
