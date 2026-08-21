35a1a1e9ef59e77df092ee49bfbbdfe79a12239c
Sujeet Kumar <sk7295911@gmail.com>
2026-08-21 15:40:24 +0530

    Add Java Solution to Patterns/Two Pointers

--- LeetCode/Patterns/Two Pointers/287. Find the Duplicate Number/Solution.java
+++ LeetCode/Patterns/Two Pointers/287. Find the Duplicate Number/Solution.java
@@ -1,19 +1,13 @@
 class Solution {
     public int findDuplicate(int[] arr) {
-        int n=arr.length-1;
-        int i=0;
-        while(i<arr.length){
-            if(arr[i]==i+1) i++;
-            else if (arr[i]==arr[arr[i]-1]) {
-                return arr[i];
-            }
-            else{
-                int temp=arr[i]-1;
-                arr[i]=arr[temp];
-                arr[temp]=temp+1;
-            }
+        HashSet<Integer>set=new HashSet<>();
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            int val=arr[i];
+            if(set.contains(val)) ans=val;
+            else set.add(val);
         }
-        return n;
+        return ans;
         
     }
 }
