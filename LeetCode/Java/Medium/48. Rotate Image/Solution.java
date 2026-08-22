9ac36ed012a6618d3442212a97834ad89cebe128
Sujeet Kumar <sk7295911@gmail.com>
2026-08-22 16:10:19 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/48. Rotate Image/Solution.java
+++ LeetCode/Java/Medium/48. Rotate Image/Solution.java
@@ -1,26 +1,23 @@
 class Solution {
     public void rotate(int[][] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[0].length; j++) {
-                if (j>i) {
-                    int temp = arr[i][j];
-                    arr[i][j] = arr[j][i];
-                    arr[j][i] = temp;
-                }
+
+        //transpose
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<i;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
             }
         }
 
-        for (int row=0;row<arr.length;row++) {
-            int i=0,j=arr[0].length-1;
-            while(i<j){
-                int temp=arr[row][i];
-                arr[row][i]=arr[row][j];
-                arr[row][j]=temp;
-                i++;
-                j--;
-                System.out.print(arr[i][j]+" ");
+        //reverse
+        for(int i=0;i<arr.length;i++){
+            int j=arr[i].length-1,k=0;
+            while(j>k){
+                int temp=arr[i][j];
+                arr[i][j--]=arr[i][k];
+                arr[i][k++]=temp;
             }
-            System.out.println();
         }
     }
 }
