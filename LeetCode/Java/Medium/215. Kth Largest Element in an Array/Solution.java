20c46da403d99f5d4a3ba07c7c5ca7b680faba1f
Sujeet Kumar <sk7295911@gmail.com>
2026-08-22 14:09:12 +0530

    Add Medium Java Solution from LeetCode

--- LeetCode/Java/Medium/215. Kth Largest Element in an Array/Solution.java
+++ LeetCode/Java/Medium/215. Kth Largest Element in an Array/Solution.java
@@ -1,6 +1,11 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+    public int findKthLargest(int[] arr, int k) {
+        PriorityQueue<Integer>pq=new PriorityQueue(Collections.reverseOrder());
+        for(int ele:arr) pq.add(ele);
+        int val=0;
+        for(int i=1;i<=k;i++){
+            val=pq.poll();
+        }
+        return val;
     }
 }
