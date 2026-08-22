c182ce3df446269cdf97dd8d1517a3aeec5d8188
Sujeet Kumar <sk7295911@gmail.com>
2026-08-22 14:56:25 +0530

    Add Easy Java Solution from LeetCode

---
+++ LeetCode/Java/Easy/898. Transpose Matrix/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] transpose(int[][] arr) {
+        int[][]matrix=new int[arr[0].length][arr.length];
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                matrix[j][i]=arr[i][j];
+            }
+        }
+        return matrix;
+        
+    }
+}
