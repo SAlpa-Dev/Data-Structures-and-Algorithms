b4874f7f97ec9288307ee47ed0ad0e6951b8902c
Sujeet Kumar <sk7295911@gmail.com>
2026-08-23 15:41:22 +0530

    Add Java Solution to Patterns/Binary Search

--- LeetCode/Patterns/Binary Search/882. Peak Index in a Mountain Array/Solution.java
+++ LeetCode/Patterns/Binary Search/882. Peak Index in a Mountain Array/Solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+        int idx=0;
         int i=0,j=arr.length-1;
-        int peak=0;
         while(i<=j){
             int mid=i+(j-i)/2;
             if(arr[mid]>arr[mid+1]){
-                peak=mid;
+                idx=mid;
                 j=mid-1;
             }
-            else{
-                i=mid+1;
-            }
+            else i=mid+1;
         }
-        return peak;
+        return idx;
     }
 }
