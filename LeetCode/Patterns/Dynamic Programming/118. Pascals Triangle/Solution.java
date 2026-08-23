de2a2f2fb944327bb166785e84c79a29d92deb0c
Sujeet Kumar <sk7295911@gmail.com>
2026-08-23 13:22:21 +0530

    Add Java Solution to Patterns/Dynamic Programming

--- LeetCode/Patterns/Dynamic Programming/118. Pascals Triangle/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/118. Pascals Triangle/Solution.java
@@ -4,9 +4,9 @@ class Solution {
         for(int i=0;i<n;i++){
             ans.add(new ArrayList<>());
             for(int j=0;j<=i;j++){
-                if(j==0 || i==j) ans.get(i).add(1);
+                if(i==j || j==0) ans.get(i).add(1);
                 else{
-                    int val=ans.get(i-1).get(j-1) + ans.get(i-1).get(j);
+                    int val=ans.get(i-1).get(j-1)+ans.get(i-1).get(j);
                     ans.get(i).add(val);
                 }
             }
