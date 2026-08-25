a01f9067a608ea7d546c180c7cb81c0f1c7b07f9
Sujeet Kumar <sk7295911@gmail.com>
2026-08-25 23:02:45 +0530

    Add Java Solution to Patterns/Two Pointers

---
+++ LeetCode/Patterns/Two Pointers/443. String Compression/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int compress(char[] chars) {
+        if(chars.length==1) return 1;
+        int i=0,idx=0;
+        while(i<chars.length){
+            int grp_len=1;
+            char ch=chars[i];
+            while(i<chars.length-1 && chars[i]==chars[i+1]){
+                grp_len++;
+                i++;
+            }
+            chars[idx++]=ch;
+            if(grp_len>1){
+                for(char ele:String.valueOf(grp_len).toCharArray()){
+                    chars[idx++]=ele;
+                }
+            }
+            i++;
+        }
+        return idx;
+    }
+}
