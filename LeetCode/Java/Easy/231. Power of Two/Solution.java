951105f670637bb81ab4acce8fd9068372423d21
Sujeet Kumar <sk7295911@gmail.com>
2026-08-29 10:57:55 +0530

    Add Easy Java Solution for 231. Power of Two from LeetCode

--- LeetCode/Java/Easy/231. Power of Two/Solution.java
+++ LeetCode/Java/Easy/231. Power of Two/Solution.java
@@ -1,15 +1,8 @@
 class Solution {
-    public int check(int n){
-        if(n%2==0) return 0;
-        if(n%2!=0) return 1;
-        int ans=check(n/2);
-        return ans;
-    }
     public boolean isPowerOfTwo(int n) {
         if(n==0) return false;
         if(n==1) return true;
-        int val=check(n);
-        if(val%2==0) return true;
-        return false;
+        if(n%2!=0) return false;
+        return isPowerOfTwo(n/2);
     }
 }
