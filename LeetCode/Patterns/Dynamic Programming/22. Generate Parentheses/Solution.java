8ebdaf5c523752248ecdf8eaa99fbf12b9b5977e
Sujeet Kumar <sk7295911@gmail.com>
2026-08-30 14:38:53 +0530

    Add Medium Java Solution for 22. Generate Parentheses from LeetCode

--- LeetCode/Patterns/Dynamic Programming/22. Generate Parentheses/Solution.java
+++ LeetCode/Patterns/Dynamic Programming/22. Generate Parentheses/Solution.java
@@ -1,15 +1,16 @@
 class Solution {
-    public static void generate(int n,int left,int right,String ans,ArrayList<String>al){
+    public void generate(int n,String s,List<String>ans,int left,int right){
         if(right==n){
-            al.add(ans);
+            ans.add(s);
             return;
         }
-        if(left<n) generate(n,left+1,right,ans+"(",al);
-        if(right<left) generate(n,left,right+1,ans+")",al);
+        if(left<n) generate(n,s+"(",ans,left+1,right);
+        if(right<left) generate(n,s+")",ans,left,right+1);
     }
     public List<String> generateParenthesis(int n) {
-        ArrayList<String>al=new ArrayList<>();
-        generate(n,0,0,"",al);
-        return al;
+        List<String>ans=new ArrayList<>();
+        generate(n,"",ans,0,0);
+        return ans;
+
     }
 }
