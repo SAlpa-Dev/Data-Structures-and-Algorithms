3216bae83ba25a2e18fc69f5e667cec77d609528
Sujeet Kumar <sk7295911@gmail.com>
2026-09-01 16:46:36 +0530

    Add Medium Java Solution for 62. Unique Paths from LeetCode

--- LeetCode/Java/Medium/62. Unique Paths/Solution.java
+++ LeetCode/Java/Medium/62. Unique Paths/Solution.java
@@ -1,15 +1,6 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp=new int[2][n];
-        if(m==1) return 1;
-        for(int j=0;j<n;j++) dp[0][j]=1;
-        dp[1][0]=1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[1][j]=dp[1][j-1]+dp[0][j];
-                dp[0][j]=dp[1][j];
-            }
-        }
-        return dp[1][n-1];
+        if(m==1 || n==1) return 1;
+        return uniquepaths(m-1,n) +uniquePaths(m,n-1); 
     }
 }
