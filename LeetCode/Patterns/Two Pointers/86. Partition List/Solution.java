86a4e71927a63a9012334fb489b1f30452f7c45f
Sujeet Kumar <sk7295911@gmail.com>
2026-09-05 16:26:44 +0530

    Add Medium Java Solution for 86. Partition List from LeetCode

--- LeetCode/Patterns/Two Pointers/86. Partition List/Solution.java
+++ LeetCode/Patterns/Two Pointers/86. Partition List/Solution.java
@@ -10,25 +10,25 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head==null && head.next==null) return head;
-        ListNode dummy1=new ListNode(-1); 
-        ListNode dummy2=new ListNode(-1); 
-        ListNode t=head;
+        if(head==null ||head.next==null) return head;
+        ListNode dummy1=new ListNode(-1);
+        ListNode dummy2=new ListNode(-1);
         ListNode t1=dummy1;
         ListNode t2=dummy2;
+        ListNode t=head;
         while(t!=null){
-            if(t.val<x){
-                t1.next=t;
-                t1=t1.next;
-            }
-            else{
+            if(t.val>=x){
                 t2.next=t;
                 t2=t2.next;
             }
+            else{
+                t1.next=t;
+                t1=t1.next;
+            }
             t=t.next;
         }
-        t1.next=dummy2.next;
         t2.next=null;
+        t1.next=dummy2.next;
         return dummy1.next;
     }
 }
