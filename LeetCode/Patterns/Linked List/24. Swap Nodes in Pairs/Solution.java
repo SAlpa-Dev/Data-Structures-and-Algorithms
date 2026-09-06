0f0b5a679667cfba3d51a3bc21f329cb0f5f7c23
Sujeet Kumar <sk7295911@gmail.com>
2026-09-06 11:17:12 +0530

    Add Medium Java Solution for 24. Swap Nodes in Pairs from LeetCode

--- LeetCode/Patterns/Linked List/24. Swap Nodes in Pairs/Solution.java
+++ LeetCode/Patterns/Linked List/24. Swap Nodes in Pairs/Solution.java
@@ -10,14 +10,15 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        if(head==null || head.next==null) return head;
-        ListNode t=head;
-        while(t!=null && t.next!=null){
-            int temp=t.next.val;
-            t.next.val=t.val;
-            t.val=temp;
-            t=t.next.next;
+        if(head==null ||head.next==null) return head;
+        ListNode temp=head;
+        while(temp!=null && temp.next!=null){
+            int t=temp.val;
+            temp.val=temp.next.val;
+            temp.next.val=t;
+            temp=temp.next.next;
         }
+
         return head;
     }
 }
