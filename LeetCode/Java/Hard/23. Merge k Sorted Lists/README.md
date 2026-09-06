b61c47ba5d7e4e7b1f01d35fc96ee342a1edb739
Sujeet Kumar <sk7295911@gmail.com>
2026-09-06 13:14:08 +0530

    Add Hard Java Solution for 23. Merge k Sorted Lists from LeetCode

--- LeetCode/Java/Hard/23. Merge k Sorted Lists/README.md
+++ LeetCode/Java/Hard/23. Merge k Sorted Lists/README.md
@@ -8,7 +8,7 @@
 Linked List, Divide and Conquer, Heap (Priority Queue), Merge Sort, Tournament Sort
 
 ### 🚀 Performance
-- **Runtime:** 163 ms
+- **Runtime:** 6 ms
 - **Memory:** 46.9 MB
 
 ---
--- LeetCode/Java/Hard/23. Merge k Sorted Lists/Solution.java
+++ LeetCode/Java/Hard/23. Merge k Sorted Lists/Solution.java
@@ -9,50 +9,21 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        if(list1==null && list2==null) return null;
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length==0) return null;
+        PriorityQueue<ListNode>pq=new PriorityQueue<>((a,b)->a.val-b.val);
+        for(ListNode ele:lists){
+            if(ele!=null) pq.add(ele);
+        }
         ListNode dummy=new ListNode(-1);
         ListNode t=dummy;
-        ListNode t1=list1;
-        ListNode t2=list2;
-
-        while(t1!=null && t2!=null){
-            if(t1.val>=t2.val){
-                t.next=t2;
-                t=t.next;
-                t2=t2.next;
-            }
-            else{
-                t.next=t1;
-                t=t.next;
-                t1=t1.next;
-            }
-        }
-        while(t1!=null){
-            t.next=t1;
+        while(pq.size()!=0){
+            ListNode temp=pq.remove();
+            t.next=temp;
             t=t.next;
-            t1=t1.next;
-        }
-        while(t2!=null){
-            t.next=t2;
-            t=t.next;
-            t2=t2.next;
+            if(temp.next!=null) pq.add(temp.next);
         }
 
         return dummy.next;
-    }
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0) return null;
-        ArrayList<ListNode>ans=new ArrayList<>();
-        for(ListNode ele:lists) ans.add(ele);
-        while(ans.size()>1){
-            ListNode a=ans.get(ans.size()-1);
-            ans.remove(ans.size()-1);
-            ListNode b=ans.get(ans.size()-1);
-            ans.remove(ans.size()-1);
-            ListNode c=mergeTwoLists(a,b);
-            ans.add(c);
-        }
-        return ans.get(0);
     }
 }
