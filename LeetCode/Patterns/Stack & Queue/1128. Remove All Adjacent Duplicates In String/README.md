bba7baf3621b78497239847a6bf79bc150c96b95
Sujeet Kumar <sk7295911@gmail.com>
2026-09-08 13:40:28 +0530

    Add Easy Java Solution for 1128. Remove All Adjacent Duplicates In String from LeetCode

--- LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/README.md
+++ LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/README.md
@@ -9,7 +9,7 @@ String, Stack
 
 ### 🚀 Performance
 - **Runtime:** 37 ms
-- **Memory:** 47.1 MB
+- **Memory:** 47.3 MB
 
 ---
 
--- LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/Solution.java
+++ LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/Solution.java
@@ -3,10 +3,13 @@ class Solution {
         if(s.length()==1) return s;
         Stack<Character>st=new Stack<>();
         int i=0;
-        while(i<s.length()){    
+        while(i<s.length()){
             char ch=s.charAt(i);
-            if(st.size()==0 || ch!=st.peek()) st.push(ch);
-            else st.pop();
+            if(st.size()==0) st.push(ch);
+            else{
+                if(ch==st.peek()) st.pop();
+                else st.push(ch);
+            }
             i++;
         }
         StringBuilder sb=new StringBuilder();
