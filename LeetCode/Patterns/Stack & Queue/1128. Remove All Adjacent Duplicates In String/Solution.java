330909305981e9e99c6fcede41976f6684670053
Sujeet Kumar <sk7295911@gmail.com>
2026-09-08 13:20:51 +0530

    Add Easy Java Solution for 1128. Remove All Adjacent Duplicates In String from LeetCode

---
+++ LeetCode/Patterns/Stack & Queue/1128. Remove All Adjacent Duplicates In String/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        if(s.length()==1) return s;
+        Stack<Character>st=new Stack<>();
+        int i=0;
+        while(i<s.length()){
+            char ch=s.charAt(i);
+            if(st.size()==0 || st.peek()!=ch) st.push(ch);
+            else st.pop();
+            i++;
+        }
+        StringBuilder ans=new StringBuilder();
+        while(!st.isEmpty()) ans.append(st.pop());
+        String s1=ans.reverse().toString();
+        return s1;
+    }
+}
