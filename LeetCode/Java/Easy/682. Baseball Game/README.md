7803c08cffcf241dabf586273d23b9f648fa9a3f
Sujeet Kumar <sk7295911@gmail.com>
2026-09-08 14:10:00 +0530

    Add Easy Java Solution for 682. Baseball Game from LeetCode

--- LeetCode/Java/Easy/682. Baseball Game/README.md
+++ LeetCode/Java/Easy/682. Baseball Game/README.md
@@ -8,8 +8,8 @@
 Array, Stack, Simulation
 
 ### 🚀 Performance
-- **Runtime:** 3 ms
-- **Memory:** 43.7 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Java/Easy/682. Baseball Game/Solution.java
+++ LeetCode/Java/Easy/682. Baseball Game/Solution.java
@@ -4,21 +4,21 @@ class Solution {
         int i=0;
         while(i<arr.length){
             String s=arr[i];
-            if(s.equals("C")) st.pop();
-            else if(s.equals("D")) st.push(2*st.peek());
-            else if(s.equals("+")){
-                int a=st.peek();
-                st.pop();
+            if(s.equals("+")){
+                int a=st.pop();
                 int b=st.peek();
                 st.push(a);
                 int sum=a+b;
                 st.push(sum);
             }
+            else if(s.equals("D")) st.push(2*st.peek());
+            else if(s.equals("C")) st.pop();
             else st.push(Integer.parseInt(s));
             i++;
         }
-        int sum=0;
-        while(!st.isEmpty()) sum+=st.pop();
-        return sum;
+        int record=0;
+        while(!st.isEmpty()) record+=st.pop();
+
+        return record;
     }
 }
