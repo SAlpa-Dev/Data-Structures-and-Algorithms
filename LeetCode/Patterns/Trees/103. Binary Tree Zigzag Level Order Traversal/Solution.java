eb73fe5712d298076db9f12da6adc36288e3ca9b
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-11 16:53:11 +0530

    Add Medium Java Solution for 103. Binary Tree Zigzag Level Order Traversal from LeetCode

--- LeetCode/Patterns/Trees/103. Binary Tree Zigzag Level Order Traversal/Solution.java
+++ LeetCode/Patterns/Trees/103. Binary Tree Zigzag Level Order Traversal/Solution.java
@@ -12,33 +12,35 @@
  *         this.right = right;
  *     }
  * }
- */
+*/
 class Pair{
     TreeNode node;
     int level;
+
     Pair(TreeNode node,int level){
         this.node=node;
         this.level=level;
     }
 }
+
 class Solution {
-    public void bfsTraversal(TreeNode root,List<List<Integer>>ans){
+    public void traversal(TreeNode root,List<List<Integer>>ans){
         if(root==null) return;
         Queue<Pair>q=new LinkedList<>();
         q.add(new Pair(root,0));
         while(!q.isEmpty()){
-            Pair front=q.poll();
+            Pair front=q.remove();
             int level=front.level;
             if(ans.size()==level) ans.add(new ArrayList<>());
-            if(level%2==0) ans.get(level).add(front.node.val);
-            else ans.get(level).add(0,front.node.val);
+            if(level%2!=0) ans.get(level).add(0,front.node.val);
+            else ans.get(level).add(front.node.val);
             if(front.node.left!=null) q.add(new Pair(front.node.left,level+1));
             if(front.node.right!=null) q.add(new Pair(front.node.right,level+1));
         }
     }
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>>ans=new ArrayList<>();
-        bfsTraversal(root,ans);
+        traversal(root,ans);
         return ans;
     }
 }
