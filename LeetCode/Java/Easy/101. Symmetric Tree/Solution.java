08e542d63321c371007b6798ef0d41eaa32b4b35
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-11 14:36:54 +0530

    Add Easy Java Solution for 101. Symmetric Tree from LeetCode

--- LeetCode/Java/Easy/101. Symmetric Tree/Solution.java
+++ LeetCode/Java/Easy/101. Symmetric Tree/Solution.java
@@ -14,22 +14,13 @@
  * }
  */
 class Solution {
-    void mirror(TreeNode root) {
-        if(root==null) return;
-        TreeNode temp=root.right;
-        root.right=root.left;
-        root.left=temp;
-        mirror(root.left);
-        mirror(root.right);
-    }
-    public boolean isSameTree(TreeNode p, TreeNode q) {
+    public boolean isSame(TreeNode p,TreeNode q){
         if(p==null && q==null) return true;
         if(p==null || q==null) return false;
         if(p.val!=q.val) return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return isSame(p.left,q.right) && isSame(p.right,q.left);
     }
     public boolean isSymmetric(TreeNode root) {
-        mirror(root.left);
-        return isSameTree(root.left,root.right);
+        return isSame(root.left,root.right);
     }
 }
