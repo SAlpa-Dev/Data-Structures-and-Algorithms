af44f8167a1955f008e6b31ce817c39bd1ceeb8d
Sujeet Kumar <sujeet.13kr@gmail.com>
2026-09-12 15:47:42 +0530

    Add Medium Java Solution for 236. Lowest Common Ancestor of a Binary Tree from LeetCode

--- LeetCode/Patterns/Trees/236. Lowest Common Ancestor of a Binary Tree/Solution.java
+++ LeetCode/Patterns/Trees/236. Lowest Common Ancestor of a Binary Tree/Solution.java
@@ -13,7 +13,7 @@ class Solution {
         if(root==p || root==q) return root;
         TreeNode left=lowestCommonAncestor(root.left,p,q);
         TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left!=null && right!=null) return root;
-        return left==null? right:left;
+        if(left==p && right==q) return root;
+        return left;
     }
 }
